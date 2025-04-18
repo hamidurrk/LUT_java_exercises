5052671a73225337fbf7d5c92e3e0b30897d2f54
hamidurrk <hamidurrk@gmail.com>
2025-04-18 23:48:49 +0300

    Add initial project structure with Maven configuration and basic Java classes

---
+++ practice/src/main/java/com/study/App.java
@@ -0,0 +1,14 @@
+package com.study;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello " + name);
+    }
+}
---
+++ practice/src/main/java/com/study/DataTypes.java
@@ -0,0 +1,22 @@
+package com.study;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        String string = new String("Hello!");
+        int x = 5;
+        Integer y = 6; 
+        System.out.println(string);
+        System.out.println(x);
+        System.out.println(y);
+
+        // Primitive types
+        // boolean
+        // byte
+        // char
+        // short
+        // long
+        // int
+        // float
+        // double
+    }
+}
